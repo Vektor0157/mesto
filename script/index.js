21c5601376ef44d42d005fc22225bd13cf471246
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2023-02-16 18:24:28 +0400

    первая сдача проекта №4: Mesto

---
+++ script/index.js
@@ -0,0 +1,34 @@
+let popupOpenBtn = document.querySelector('.profile__btn');
+let popup = document.querySelector('.popup');
+let popupCloseBtn = document.querySelector('.popup__close-icon');
+let userInputName = document.querySelector('.popup__input-name');
+let userInputInfo = document.querySelector('.popup__input-info');
+let popupForm = document.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
+function openPopup(){
+	popup.classList.add('popup_open');
+	userInputName.value = profileName.textContent;
+	userInputInfo.value = profileDescription.textContent;
+}
+
+function closePopup(){
+	popup.classList.remove('popup_open');
+}
+
+function savePopup(evt){
+	evt.preventDefault();
+	profileName.textContent = userInputName.value;
+	profileDescription.textContent = userInputInfo.value;
+	closePopup();
+}
+
+if (popup.classList.contains('popup_open') === false) {
+	popupOpenBtn.addEventListener('click', openPopup);
+}
+
+
+popupCloseBtn.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', savePopup);
